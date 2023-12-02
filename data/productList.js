d28900492cb8df6776098615562b91631b4c1dd5
jay <pateljay8886@gmail.com>
2023-12-02 18:14:18 +0530

    Dummy data added

--- data/productList.js
+++ data/productList.js
@@ -3,6 +3,9 @@ import two from '../assets/2.jpg';
 import three from '../assets/3.jpg';
 import four from '../assets/4.jpg';
 import five from '../assets/5.jpg';
+import Six from '../assets/6.jpg';
+import Seven from '../assets/7.jpg';
+import eight from '../assets/8.jpg';
 
 const productList = [
   { id: '1', name: 'Headphone', imageUrl: one, description: 'High quality wireless headphones with noise cancellation.', price: '₹7,499' },
@@ -10,6 +13,10 @@ const productList = [
   { id: '3', name: 'Sunglasses', imageUrl: three, description: 'Stylish sunglasses with UV protection.', price: '₹3,749' },
   { id: '4', name: 'Bottle', imageUrl: four, description: 'Durable and leak-proof water bottle.', price: '₹1,499' },
   { id: '5', name: 'Gameboy', imageUrl: five, description: 'Classic handheld gaming console.', price: '₹11,249' },
+  { id: '6', name: 'Speaker', imageUrl: five, description: 'High-quality portable speaker with crystal clear sound.', price: '₹2,000' },
+  { id: '7', name: 'Controller', imageUrl: five, description: 'Responsive and ergonomic gaming controller for ultimate gaming experience.', price: '₹7,000' },
+  { id: '8', name: 'Smartwatch', imageUrl: five, description: 'Feature-rich smartwatch with fitness tracking and notifications.', price: '₹12,000' },
+
   // Add more products as needed
 ];
 
